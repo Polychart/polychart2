bc71e11b332696acd8ebfe41219e9866cb5944f8
Lisa Zhang <lz.weiqi@gmail.com>
2012-12-01 11:30:22 -0500

    make renderers OO and more coordinate aware

--- compiled/src/render.js
+++ compiled/src/render.js
@@ -1,5 +1,7 @@
 (function() {
-  var poly, renderer, _makePath, _maybeApply;
+  var Circle, HLine, Line, Renderer, Text, VLine, poly, renderer,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   poly = this.poly || {};
 
@@ -23,7 +25,7 @@
     return {
       add: function(mark, evtData) {
         var pt;
-        pt = renderer[mark.type].render(paper, scales, mark);
+        pt = renderer.cartesian[mark.type].render(paper, scales, mark);
         pt.attr('clip-rect', clipping);
         pt.click(function() {
           return eve(id + ".click", this, evtData);
@@ -37,9 +39,7 @@
         return pt.remove();
       },
       animate: function(pt, mark, evtData) {
-        var attr;
-        attr = renderer[mark.type].attr(scales, mark);
-        pt.animate(attr, 300);
+        renderer.cartesian[mark.type].animate(pt, scales, mark);
         pt.unclick();
         pt.click(function() {
           return eve(id + ".click", this, evtData);
@@ -53,145 +53,202 @@
     };
   };
 
-  _maybeApply = function(scale, value) {
-    if (scale != null) {
-      return scale(value);
-    } else if (_.isObject(value)) {
-      return value.v;
-    } else {
-      return value;
+  Renderer = (function() {
+
+    function Renderer() {}
+
+    Renderer.prototype.render = function(paper, scales, mark) {
+      var pt;
+      pt = this._make(paper);
+      _.each(this.attr(scales, mark), function(v, k) {
+        return pt.attr(k, v);
+      });
+      return pt;
+    };
+
+    Renderer.prototype._make = function() {
+      throw new poly.NotImplemented();
+    };
+
+    Renderer.prototype.animate = function(pt, scales, mark) {
+      return pt.animate(this.attr(scales, mark), 300);
+    };
+
+    Renderer.prototype.attr = function(scales, mark) {
+      throw new poly.NotImplemented();
+    };
+
+    Renderer.prototype._makePath = function(xs, ys, type) {
+      var path;
+      if (type == null) type = 'L';
+      path = _.map(xs, function(x, i) {
+        return (i === 0 ? 'M' : type) + x + ' ' + ys[i];
+      });
+      return path.join(' ') + 'Z';
+    };
+
+    Renderer.prototype._maybeApply = function(scale, val) {
+      if (scale != null) {
+        return scale(val);
+      } else if (_.isObject(val)) {
+        return val.v;
+      } else {
+        return val;
+      }
+    };
+
+    return Renderer;
+
+  })();
+
+  Circle = (function(_super) {
+
+    __extends(Circle, _super);
+
+    function Circle() {
+      Circle.__super__.constructor.apply(this, arguments);
     }
-  };
+
+    Circle.prototype._make = function(paper) {
+      return paper.circle();
+    };
+
+    Circle.prototype.attr = function(scales, mark) {
+      return {
+        cx: scales.x(mark.x),
+        cy: scales.y(mark.y),
+        r: this._maybeApply(scales.size, mark.size),
+        fill: this._maybeApply(scales.color, mark.color),
+        stroke: this._maybeApply(scales.color, mark.color),
+        'stroke-width': '0px'
+      };
+    };
+
+    return Circle;
+
+  })(Renderer);
+
+  Line = (function(_super) {
+
+    __extends(Line, _super);
+
+    function Line() {
+      Line.__super__.constructor.apply(this, arguments);
+    }
+
+    Line.prototype._make = function(paper) {
+      return paper.path();
+    };
+
+    Line.prototype.attr = function(scales, mark) {
+      var xs, ys;
+      xs = _.map(mark.x, scales.x);
+      ys = _.map(mark.y, scales.y);
+      return {
+        path: this._makePath(xs, ys),
+        stroke: 'black'
+      };
+    };
+
+    return Line;
+
+  })(Renderer);
+
+  HLine = (function(_super) {
+
+    __extends(HLine, _super);
+
+    function HLine() {
+      HLine.__super__.constructor.apply(this, arguments);
+    }
+
+    HLine.prototype._make = function(paper) {
+      return paper.path();
+    };
+
+    HLine.prototype.attr = function(scales, mark) {
+      var y;
+      y = scales.y(mark.y);
+      return {
+        path: this._makePath([0, 100000], [y, y]),
+        stroke: 'black',
+        'stroke-width': '1px'
+      };
+    };
+
+    return HLine;
+
+  })(Renderer);
+
+  VLine = (function(_super) {
+
+    __extends(VLine, _super);
+
+    function VLine() {
+      VLine.__super__.constructor.apply(this, arguments);
+    }
+
+    VLine.prototype._make = function(paper) {
+      return paper.path();
+    };
+
+    VLine.prototype.attr = function(scales, mark) {
+      var x;
+      x = scales.x(mark.x);
+      return {
+        path: this._makePath([x, x], [0, 100000]),
+        stroke: 'black',
+        'stroke-width': '1px'
+      };
+    };
+
+    return VLine;
+
+  })(Renderer);
+
+  Text = (function(_super) {
+
+    __extends(Text, _super);
+
+    function Text() {
+      Text.__super__.constructor.apply(this, arguments);
+    }
+
+    Text.prototype._make = function(paper) {
+      return paper.text();
+    };
+
+    Text.prototype.attr = function(scales, mark) {
+      var _ref;
+      return {
+        x: scales.x(mark.x),
+        y: scales.y(mark.y),
+        text: this._maybeApply(scales.text, mark.text),
+        'text-anchor': (_ref = mark['text-anchor']) != null ? _ref : 'left',
+        r: 10,
+        fill: 'black'
+      };
+    };
+
+    return Text;
+
+  })(Renderer);
 
   renderer = {
-    circle: {
-      render: function(paper, scales, mark) {
-        var pt;
-        pt = paper.circle();
-        _.each(renderer.circle.attr(scales, mark), function(v, k) {
-          return pt.attr(k, v);
-        });
-        return pt;
-      },
-      attr: function(scales, mark) {
-        return {
-          cx: scales.x(mark.x),
-          cy: scales.y(mark.y),
-          r: _maybeApply(scales.size, mark.size),
-          fill: _maybeApply(scales.color, mark.color),
-          stroke: _maybeApply(scales.color, mark.color),
-          'stroke-width': '0px'
-        };
-      },
-      animate: function(pt, scales, mark) {
-        return pt.animate(attr);
-      }
+    cartesian: {
+      circle: new Circle(),
+      line: new Line(),
+      hline: new HLine(),
+      vline: new VLine(),
+      text: new Text()
     },
-    line: {
-      render: function(paper, scales, mark) {
-        var pt;
-        pt = paper.path();
-        _.each(renderer.line.attr(scales, mark), function(v, k) {
-          return pt.attr(k, v);
-        });
-        return pt;
-      },
-      attr: function(scales, mark) {
-        var xs, ys;
-        xs = _.map(mark.x, scales.x);
-        ys = _.map(mark.y, scales.y);
-        return {
-          path: _makePath(xs, ys),
-          stroke: 'black'
-        };
-      },
-      animate: function(pt, scales, mark) {
-        return pt.animate(attr);
-      }
-    },
-    hline: {
-      render: function(paper, scales, mark) {
-        var pt;
-        pt = paper.path();
-        _.each(renderer.hline.attr(scales, mark), function(v, k) {
-          return pt.attr(k, v);
-        });
-        return pt;
-      },
-      attr: function(scales, mark) {
-        var y;
-        y = scales.y(mark.y);
-        return {
-          path: _makePath([0, 100000], [y, y]),
-          stroke: 'black',
-          'stroke-width': '1px'
-        };
-      },
-      animate: function(pt, scales, mark) {
-        return pt.animate(attr);
-      }
-    },
-    vline: {
-      render: function(paper, scales, mark) {
-        var pt;
-        pt = paper.path();
-        _.each(renderer.vline.attr(scales, mark), function(v, k) {
-          return pt.attr(k, v);
-        });
-        return pt;
-      },
-      attr: function(scales, mark) {
-        var x;
-        x = scales.x(mark.x);
-        return {
-          path: _makePath([x, x], [0, 100000]),
-          stroke: 'black',
-          'stroke-width': '1px'
-        };
-      },
-      animate: function(pt, scales, mark) {
-        return pt.animate(attr);
-      }
-    },
-    text: {
-      render: function(paper, scales, mark) {
-        var pt;
-        pt = paper.text();
-        _.each(renderer.text.attr(scales, mark), function(v, k) {
-          return pt.attr(k, v);
-        });
-        return pt;
-      },
-      attr: function(scales, mark) {
-        var _ref;
-        return {
-          x: scales.x(mark.x),
-          y: scales.y(mark.y),
-          text: _maybeApply(scales.text, mark.text),
-          'text-anchor': (_ref = mark['text-anchor']) != null ? _ref : 'left',
-          r: 10,
-          fill: 'black'
-        };
-      },
-      animate: function(pt, scales, mark) {
-        return pt.animate(attr);
-      }
+    polar: {
+      circle: new Circle(),
+      line: new Line(),
+      hline: new HLine(),
+      vline: new VLine(),
+      text: new Text()
     }
-  };
-
-  _makePath = function(xs, ys) {
-    var str;
-    str = '';
-    _.each(xs, function(x, i) {
-      var y;
-      y = ys[i];
-      if (str === '') {
-        return str += 'M' + x + ' ' + y;
-      } else {
-        return str += ' L' + x + ' ' + y;
-      }
-    });
-    return str + ' Z';
   };
 
 }).call(this);
--- src/render.coffee
+++ src/render.coffee
@@ -14,7 +14,7 @@ TODO:
 ###
 poly.render = (id, paper, scales, clipping) ->
   add: (mark, evtData) ->
-    pt = renderer[mark.type].render paper, scales, mark
+    pt = renderer.cartesian[mark.type].render paper, scales, mark
     pt.attr('clip-rect', clipping)
     pt.click () -> eve(id+".click", @, evtData)
     pt.hover () -> eve(id+".hover", @, evtData)
@@ -22,84 +22,82 @@ poly.render = (id, paper, scales, clipping) ->
   remove: (pt) ->
     pt.remove()
   animate: (pt, mark, evtData) ->
-    attr = renderer[mark.type].attr scales, mark
-    pt.animate attr, 300
+    renderer.cartesian[mark.type].animate pt, scales, mark
     pt.unclick() # <-- ?!?!?!
     pt.click () -> eve(id+".click", @, evtData)
     pt.unhover() # <-- ?!?!?!
     pt.hover () -> eve(id+".hover", @, evtData)
     pt
 
-_maybeApply = (scale, value) ->
-  if scale? then scale(value) else if _.isObject(value) then value.v else value
+class Renderer
+  constructor : ->
+  render: (paper, scales, mark) ->
+    pt = @_make(paper)
+    _.each @attr(scales, mark), (v, k) -> pt.attr(k, v)
+    pt
+  _make : () -> throw new poly.NotImplemented()
+  animate: (pt, scales, mark) -> pt.animate @attr(scales, mark), 300
+  attr: (scales, mark) -> throw new poly.NotImplemented()
+  _makePath : (xs, ys, type='L') ->
+    path = _.map xs, (x, i) -> (if i == 0 then 'M' else type) + x+' '+ys[i]
+    path.join(' ') + 'Z'
+  _maybeApply : (scale, val) ->
+    if scale? then scale(val) else if _.isObject(val) then val.v else val
+
+class Circle extends Renderer # for both cartesian & polar
+  _make: (paper) -> paper.circle()
+  attr: (scales, mark) ->
+    cx: scales.x(mark.x)
+    cy: scales.y(mark.y)
+    r: @_maybeApply scales.size, mark.size
+    fill: @_maybeApply scales.color, mark.color
+    stroke: @_maybeApply scales.color, mark.color
+    'stroke-width': '0px'
+
+class Line extends Renderer # for both cartesian & polar?
+  _make: (paper) -> paper.path()
+  attr: (scales, mark) ->
+    xs = _.map mark.x, scales.x
+    ys = _.map mark.y, scales.y
+    path: @_makePath xs, ys
+    stroke: 'black'
+
+class HLine extends Renderer # for both cartesian & polar?
+  _make: (paper) -> paper.path()
+  attr: (scales, mark) ->
+    y = scales.y mark.y
+    path: @_makePath([0, 100000], [y, y])
+    stroke: 'black'
+    'stroke-width': '1px'
+
+class VLine extends Renderer # for both cartesian & polar?
+  _make: (paper) -> paper.path()
+  attr: (scales, mark) ->
+    x = scales.x mark.x
+    path: @_makePath([x, x], [0, 100000])
+    stroke: 'black'
+    'stroke-width': '1px'
+
+class Text extends Renderer # for both cartesian & polar
+  _make: (paper) -> paper.text()
+  attr: (scales, mark) ->
+    x: scales.x(mark.x)
+    y: scales.y(mark.y)
+    text: @_maybeApply  scales.text, mark.text
+    'text-anchor' : mark['text-anchor'] ? 'left'
+    r: 10
+    fill: 'black'
 
 renderer =
-  circle:
-    render: (paper, scales, mark) ->
-      pt = paper.circle()
-      _.each renderer.circle.attr(scales, mark), (v, k) -> pt.attr(k, v)
-      pt
-    attr: (scales, mark) ->
-      # handle the case when mark.FOO does not exist
-      cx: scales.x(mark.x)
-      cy: scales.y(mark.y)
-      r: _maybeApply scales.size, mark.size
-      fill: _maybeApply scales.color, mark.color
-      stroke: _maybeApply scales.color, mark.color
-      'stroke-width': '0px'
-    animate: (pt, scales, mark) -> pt.animate attr
-  line:
-    render: (paper, scales, mark) ->
-      pt = paper.path()
-      _.each renderer.line.attr(scales, mark), (v, k) -> pt.attr(k, v)
-      pt
-    attr: (scales, mark) ->
-      xs = _.map mark.x, scales.x
-      ys = _.map mark.y, scales.y
-      path: _makePath xs, ys
-      stroke: 'black'
-    animate: (pt, scales, mark) -> pt.animate attr
-  hline:
-    render: (paper, scales, mark) ->
-      pt = paper.path()
-      _.each renderer.hline.attr(scales, mark), (v, k) -> pt.attr(k, v)
-      pt
-    attr: (scales, mark) ->
-      y = scales.y mark.y
-      path: _makePath([0, 100000], [y, y])
-      stroke: 'black'
-      'stroke-width': '1px'
-    animate: (pt, scales, mark) -> pt.animate attr
-  vline:
-    render: (paper, scales, mark) ->
-      pt = paper.path()
-      _.each renderer.vline.attr(scales, mark), (v, k) -> pt.attr(k, v)
-      pt
-    attr: (scales, mark) ->
-      x = scales.x mark.x
-      path: _makePath([x, x], [0, 100000])
-      stroke: 'black'
-      'stroke-width': '1px'
-    animate: (pt, scales, mark) -> pt.animate attr
-  text:
-    render: (paper, scales, mark) ->
-      pt = paper.text()
-      _.each renderer.text.attr(scales, mark), (v, k) -> pt.attr(k, v)
-      pt
-    attr: (scales, mark) ->
-      x: scales.x(mark.x)
-      y: scales.y(mark.y)
-      text: _maybeApply  scales.text, mark.text
-      'text-anchor' : mark['text-anchor'] ? 'left'
-      r: 10
-      fill: 'black'
-    animate: (pt, scales, mark) -> pt.animate attr
-
-_makePath = (xs, ys) ->
-  str = ''
-  _.each xs, (x, i) ->
-    y = ys[i]
-    if str == '' then str+='M'+x+' '+y
-    else str +=' L'+x+' '+y
-  str + ' Z'
-
+  cartesian:
+    circle: new Circle()
+    line: new Line()
+    hline: new HLine()
+    vline: new VLine()
+    text: new Text()
+  polar:
+    circle: new Circle()
+    line: new Line()
+    hline: new HLine()
+    vline: new VLine()
+    text: new Text()
