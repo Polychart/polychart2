d54dd3118f65977af4c9f64bf263987b8311844b
Lisa Zhang <lz.weiqi@gmail.com>
2012-12-01 14:54:01 -0500

    refactor x & y axis to be different classes

--- polychart2.js
+++ polychart2.js
@@ -668,7 +668,7 @@
 
 }).call(this);
 (function() {
-  var Axis, Guide, Legend, poly, sf,
+  var Axis, Guide, Legend, XAxis, YAxis, poly, sf,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
@@ -679,11 +679,7 @@
 
   Guide = (function() {
 
-    function Guide(params) {
-      this.guideSpec = params.guideSpec;
-      this.position = 'right';
-      this.ticks = [];
-    }
+    function Guide() {}
 
     Guide.prototype.getWidth = function() {};
 
@@ -701,15 +697,9 @@
 
     __extends(Axis, _super);
 
-    function Axis(type) {
-      this.type = type;
-      this._tickToTextFn = __bind(this._tickToTextFn, this);
-      this._tickToGeomFn = __bind(this._tickToGeomFn, this);
+    function Axis() {
       this.render = __bind(this.render, this);
-      this._renderline = __bind(this._renderline, this);
-      this.make = __bind(this.make, this);
-      this.position = this.type === 'x' ? 'bottom' : 'left';
-      this.oldticks = null;
+      this.make = __bind(this.make, this);      this.oldticks = null;
       this.rendered = false;
       this.ticks = {};
       this.pts = {};
@@ -721,31 +711,8 @@
       return this.ticks = poly.tick.make(this.domain, this.guideSpec, this.factory.tickType(this.domain));
     };
 
-    Axis.prototype._renderline = function(renderer, axisDim) {
-      var x, x1, x2, y, y1, y2;
-      if (this.type === 'x') {
-        y = sf.identity(axisDim.bottom);
-        x1 = sf.identity(axisDim.left);
-        x2 = sf.identity(axisDim.left + axisDim.width);
-        return renderer.add({
-          type: 'line',
-          y: [y, y],
-          x: [x1, x2]
-        });
-      } else {
-        x = sf.identity(axisDim.left);
-        y1 = sf.identity(axisDim.top);
-        y2 = sf.identity(axisDim.top + axisDim.height);
-        return renderer.add({
-          type: 'line',
-          x: [x, x],
-          y: [y1, y2]
-        });
-      }
-    };
-
     Axis.prototype.render = function(dim, renderer) {
-      var added, axisDim, deleted, geomfn, kept, newpts, textfn, _ref,
+      var added, axisDim, deleted, kept, newpts, _ref,
         _this = this;
       axisDim = {
         top: dim.paddingTop + dim.guideTop,
@@ -756,14 +723,12 @@
       };
       if (!this.rendered) this._renderline(renderer, axisDim);
       _ref = poly.compare(_.keys(this.pts), _.keys(this.ticks)), deleted = _ref.deleted, kept = _ref.kept, added = _ref.added;
-      geomfn = this._tickToGeomFn(axisDim);
-      textfn = this._tickToTextFn(axisDim);
       newpts = {};
       _.each(kept, function(t) {
-        return newpts[t] = _this._modify(renderer, _this.pts[t], _this.ticks[t], geomfn, textfn);
+        return newpts[t] = _this._modify(renderer, _this.pts[t], _this.ticks[t], axisDim);
       });
       _.each(added, function(t) {
-        return newpts[t] = _this._add(renderer, _this.ticks[t], geomfn, textfn);
+        return newpts[t] = _this._add(renderer, _this.ticks[t], axisDim);
       });
       _.each(deleted, function(t) {
         return _this._delete(renderer, _this.pts[t]);
@@ -772,53 +737,11 @@
       return this.rendered = true;
     };
 
-    Axis.prototype._tickToGeomFn = function(axisDim) {
-      if (this.type === 'x') {
-        return function(tick) {
-          return {
-            type: 'line',
-            x: [tick.location, tick.location],
-            y: [sf.identity(axisDim.bottom), sf.identity(axisDim.bottom + 5)]
-          };
-        };
-      }
-      return function(tick) {
-        return {
-          type: 'line',
-          x: [sf.identity(axisDim.left), sf.identity(axisDim.left - 5)],
-          y: [tick.location, tick.location]
-        };
-      };
-    };
-
-    Axis.prototype._tickToTextFn = function(axisDim) {
-      if (this.type === 'x') {
-        return function(tick) {
-          return {
-            type: 'text',
-            x: tick.location,
-            y: sf.identity(axisDim.bottom + 15),
-            text: tick.value,
-            'text-anchor': 'middle'
-          };
-        };
-      }
-      return function(tick) {
-        return {
-          type: 'text',
-          x: sf.identity(axisDim.left - 7),
-          y: tick.location,
-          text: tick.value,
-          'text-anchor': 'end'
-        };
-      };
-    };
-
-    Axis.prototype._add = function(renderer, tick, geomfn, textfn) {
+    Axis.prototype._add = function(renderer, tick, axisDim) {
       var obj;
       obj = {};
-      obj.tick = renderer.add(geomfn(tick));
-      obj.text = renderer.add(textfn(tick));
+      obj.tick = renderer.add(this._makeTick(axisDim, tick));
+      obj.text = renderer.add(this._makeText(axisDim, tick));
       return obj;
     };
 
@@ -827,17 +750,115 @@
       return renderer.remove(pt.text);
     };
 
-    Axis.prototype._modify = function(renderer, pt, tick, geomfn, textfn) {
+    Axis.prototype._modify = function(renderer, pt, tick, axisDim) {
       var obj;
       obj = [];
-      obj.tick = renderer.animate(pt.tick, geomfn(tick));
-      obj.text = renderer.animate(pt.text, textfn(tick));
+      obj.tick = renderer.animate(pt.tick, this._makeTick(axisDim, tick));
+      obj.text = renderer.animate(pt.text, this._makeText(axisDim, tick));
       return obj;
+    };
+
+    Axis.prototype._renderline = function() {
+      throw new poly.NotImplemented();
+    };
+
+    Axis.prototype._makeTick = function() {
+      throw new poly.NotImplemented();
+    };
+
+    Axis.prototype._makeText = function() {
+      throw new poly.NotImplemented();
     };
 
     return Axis;
 
   })(Guide);
+
+  XAxis = (function(_super) {
+
+    __extends(XAxis, _super);
+
+    function XAxis() {
+      this._renderline = __bind(this._renderline, this);
+      XAxis.__super__.constructor.apply(this, arguments);
+    }
+
+    XAxis.prototype._renderline = function(renderer, axisDim) {
+      var x1, x2, y;
+      y = sf.identity(axisDim.bottom);
+      x1 = sf.identity(axisDim.left);
+      x2 = sf.identity(axisDim.left + axisDim.width);
+      return renderer.add({
+        type: 'line',
+        y: [y, y],
+        x: [x1, x2]
+      });
+    };
+
+    XAxis.prototype._makeTick = function(axisDim, tick) {
+      return {
+        type: 'line',
+        x: [tick.location, tick.location],
+        y: [sf.identity(axisDim.bottom), sf.identity(axisDim.bottom + 5)]
+      };
+    };
+
+    XAxis.prototype._makeText = function(axisDim, tick) {
+      return {
+        type: 'text',
+        x: tick.location,
+        y: sf.identity(axisDim.bottom + 15),
+        text: tick.value,
+        'text-anchor': 'middle'
+      };
+    };
+
+    return XAxis;
+
+  })(Axis);
+
+  YAxis = (function(_super) {
+
+    __extends(YAxis, _super);
+
+    function YAxis() {
+      this._renderline = __bind(this._renderline, this);
+      YAxis.__super__.constructor.apply(this, arguments);
+    }
+
+    YAxis.prototype._renderline = function(renderer, axisDim) {
+      var x, y1, y2;
+      x = sf.identity(axisDim.left);
+      y1 = sf.identity(axisDim.top);
+      y2 = sf.identity(axisDim.top + axisDim.height);
+      return renderer.add({
+        type: 'line',
+        x: [x, x],
+        y: [y1, y2]
+      });
+    };
+
+    YAxis.prototype._makeTick = function(axisDim, tick) {
+      return {
+        type: 'line',
+        x: [sf.identity(axisDim.left), sf.identity(axisDim.left - 5)],
+        y: [tick.location, tick.location]
+      };
+    };
+
+    YAxis.prototype._makeText = function(axisDim, tick) {
+      return {
+        type: 'text',
+        x: sf.identity(axisDim.left - 7),
+        y: tick.location,
+        text: tick.value,
+        'text-anchor': 'end'
+      };
+    };
+
+    return YAxis;
+
+  })(Axis);
 
   Legend = (function() {
 
@@ -851,8 +872,9 @@
 
   poly.guide = {};
 
-  poly.guide.axis = function(params) {
-    return new Axis(params);
+  poly.guide.axis = function(type) {
+    if (type === 'x') return new XAxis();
+    return new YAxis();
   };
 
   this.poly = poly;
--- src/guide.coffee
+++ src/guide.coffee
@@ -3,18 +3,14 @@ poly = @poly || {}
 sf = poly.const.scaleFns
 
 class Guide
-  constructor: (params) ->
-    {@guideSpec} = params
-    @position = 'right'
-    @ticks = []
+  constructor: () ->
   getWidth: () ->  # approximate
   getHeight: () -> # approximate
   render: (paper, render, scales) ->
     throw new poly.NotImplemented("render is not implemented")
 
 class Axis extends Guide
-  constructor: (@type) ->
-    @position = if @type == 'x' then 'bottom' else 'left'
+  constructor: () ->
     @oldticks = null
     @rendered = false
     @ticks = {}
@@ -23,17 +19,6 @@ class Axis extends Guide
     {@domain, @factory, @guideSpec} = params
     @oldticks = @ticks
     @ticks = poly.tick.make @domain, @guideSpec, @factory.tickType(@domain)
-  _renderline : (renderer, axisDim) =>
-    if @type == 'x'
-      y = sf.identity axisDim.bottom
-      x1 = sf.identity axisDim.left
-      x2 = sf.identity axisDim.left+axisDim.width
-      renderer.add { type: 'line', y: [y, y], x: [x1, x2]}
-    else
-      x = sf.identity axisDim.left
-      y1 = sf.identity axisDim.top
-      y2 = sf.identity axisDim.top+axisDim.height
-      renderer.add { type: 'line', x: [x, x], y: [y1, y2] }
   render: (dim, renderer) =>
     axisDim =
       top: dim.paddingTop + dim.guideTop
@@ -44,61 +29,71 @@ class Axis extends Guide
     if !@rendered then @_renderline renderer, axisDim
 
     {deleted, kept, added} = poly.compare _.keys(@pts), _.keys(@ticks)
-    geomfn = @_tickToGeomFn axisDim
-    textfn = @_tickToTextFn axisDim
     newpts = {}
     _.each kept, (t) =>
-      newpts[t] = @_modify renderer, @pts[t], @ticks[t], geomfn, textfn
-    _.each added, (t) => newpts[t] = @_add renderer, @ticks[t], geomfn, textfn
+      newpts[t] = @_modify renderer, @pts[t], @ticks[t], axisDim
+    _.each added, (t) => newpts[t] = @_add renderer, @ticks[t], axisDim
     _.each deleted, (t) => @_delete renderer, @pts[t]
     @pts = newpts
     @rendered = true
-
-  _tickToGeomFn: (axisDim) =>
-    if @type == 'x'
-      return (tick) ->
-        type: 'line'
-        x : [tick.location, tick.location]
-        y : [sf.identity(axisDim.bottom), sf.identity(axisDim.bottom+5)]
-    (tick) ->
-      type: 'line'
-      x : [sf.identity(axisDim.left), sf.identity(axisDim.left-5)]
-      y : [tick.location, tick.location]
-
-  _tickToTextFn: (axisDim) =>
-    if @type == 'x'
-      return (tick) ->
-        type: 'text'
-        x : tick.location
-        y : sf.identity(axisDim.bottom+15)
-        text: tick.value
-        'text-anchor' : 'middle'
-    (tick) ->
-      type: 'text'
-      x : sf.identity(axisDim.left-7)
-      y : tick.location
-      text: tick.value
-      'text-anchor' : 'end'
-
-  _add: (renderer, tick, geomfn, textfn) ->
+  _add: (renderer, tick, axisDim) ->
     obj = {}
-    obj.tick = renderer.add geomfn(tick)
-    obj.text = renderer.add textfn(tick)
+    obj.tick = renderer.add @_makeTick(axisDim, tick)
+    obj.text = renderer.add @_makeText(axisDim, tick)
     obj
   _delete: (renderer, pt) ->
     renderer.remove pt.tick
     renderer.remove pt.text
-  _modify: (renderer, pt, tick, geomfn, textfn) ->
+  _modify: (renderer, pt, tick, axisDim) ->
     obj = []
-    obj.tick = renderer.animate pt.tick, geomfn(tick)
-    obj.text = renderer.animate pt.text, textfn(tick)
+    obj.tick = renderer.animate pt.tick, @_makeTick(axisDim, tick)
+    obj.text = renderer.animate pt.text, @_makeText(axisDim, tick)
     obj
+  _renderline : () -> throw new poly.NotImplemented()
+  _makeTick : () -> throw new poly.NotImplemented()
+  _makeText: () -> throw new poly.NotImplemented()
+
+class XAxis extends Axis
+  _renderline : (renderer, axisDim) =>
+    y = sf.identity axisDim.bottom
+    x1 = sf.identity axisDim.left
+    x2 = sf.identity axisDim.left+axisDim.width
+    renderer.add { type: 'line', y: [y, y], x: [x1, x2]}
+  _makeTick: (axisDim, tick) ->
+    type: 'line'
+    x : [tick.location, tick.location]
+    y : [sf.identity(axisDim.bottom), sf.identity(axisDim.bottom+5)]
+  _makeText: (axisDim, tick) ->
+    type: 'text'
+    x : tick.location
+    y : sf.identity(axisDim.bottom+15)
+    text: tick.value
+    'text-anchor' : 'middle'
+class YAxis extends Axis
+  _renderline : (renderer, axisDim) =>
+    x = sf.identity axisDim.left
+    y1 = sf.identity axisDim.top
+    y2 = sf.identity axisDim.top+axisDim.height
+    renderer.add { type: 'line', x: [x, x], y: [y1, y2] }
+  _makeTick: (axisDim, tick) ->
+    type: 'line'
+    x : [sf.identity(axisDim.left), sf.identity(axisDim.left-5)]
+    y : [tick.location, tick.location]
+  _makeText: (axisDim, tick) ->
+    type: 'text'
+    x : sf.identity(axisDim.left-7)
+    y : tick.location
+    text: tick.value
+    'text-anchor' : 'end'
 
 
 class Legend
   render: (paper, render, scales) ->
 
 poly.guide = {}
-poly.guide.axis = (params) -> new Axis(params)
+poly.guide.axis = (type) ->
+  if type == 'x'
+    return new XAxis()
+  return new YAxis()
 
 @poly = poly
